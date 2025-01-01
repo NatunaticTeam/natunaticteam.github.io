81d6766f48178fbb66fc183550494e0ed82c74a3
n4c <146791933+n4c-s@users.noreply.github.com>
2025-01-01 02:53:00 -0300

    lang support added
    added spanish and english language support, hope it works

--- js/lang.js
+++ js/lang.js
@@ -1,12 +1,20 @@
 const flagsElement = document.getElementById("flags");
 
+const textsToChange = document.querySelectorAll("[data-section]")
 const changeLanguage = async (language) => {
     const requestJson = await fetch(`../languages/${language}.json`);
     const texts = await requestJson.json();
 
-    console.log(texts);
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section
+        const value = textToChange.dataset.value
+
+        textToChange.innerHTML=texts[section][value];
+    }
 };
 
-flagsElement.addEventListener('click', (e) => {
+const langChanger = flagsElement.addEventListener('click', (e) => {
     changeLanguage(e.target.parentElement.dataset.language);
 });
+
+document.cookie("langChanger")
