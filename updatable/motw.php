fa43adb06300a8eb7b7dbe89f4e4a965100cdfc0
n4c <146791933+n4c-s@users.noreply.github.com>
2025-01-15 02:27:06 +0000

    just doing the last changes

---
+++ updatable/motw.php
@@ -0,0 +1,12 @@
+<div class="motw" id="motw">
+                <center><span class="title" data-section="motw" data-value="title">MOTW</span></center>
+                <p></p>
+                <center><img src="linebreak-xd.png" alt=" "></center>
+                <div class="motw-msg" id="motw-msg" data-section="motw" data-value="motw-msg">
+                    <center>
+                        <p></p>
+                        <p>God Eater from Introverse mod literally Best character lol</p>
+                        <p id="nmsg-date" data-section="motw" data-value="nmsg-date">(new one on 2025/01/13)</p>
+                    </center>
+                </div>
+            </div>
